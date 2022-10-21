523155d6b107d8c5bd4570484c6d4d5666bf5454
b43646 <b43646@qq.com>
2022-10-21 16:23:16 +0800

    add environment info

---
+++ environment/include.sh
@@ -0,0 +1,5 @@
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+KAFKA_DIR="${MY_DIR}/kafka-3.2.3"
+BROKER_CONFIG_DIR="${MY_DIR}/configs/kafka"
+ZOO_CONFIG_DIR="${MY_DIR}/configs/zookeeper"
+CONNECT_CONFIG_DIR="${MY_DIR}/configs/connect"
